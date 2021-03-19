3c2545e89e101c77d6184e4b7eb81d1a8ff049b3
sublimacja <zuzanna.adamiec@gmail.com>
2021-03-19 16:32:50 +0100

    zmiany w metodzie issuance - użycie klasy InvoiceRequest

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -21,10 +21,10 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = Invoice.createInvoice(Id.generate(), client);
+    public Invoice issuance(InvoiceRequest invoiceRequest) {
+        Invoice invoice = Invoice.createInvoice(Id.generate(), invoiceRequest.getClient());
 
-        for (RequestItem item : items) {
+        for (RequestItem item : invoiceRequest.getItems()) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
             String desc = null;
